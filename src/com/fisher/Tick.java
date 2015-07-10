3e0314a13194e9e94f2b3c44b7032a309a4a2a06
Nicolas Xu <xu.shenxin@gmail.com>
2015-07-10 15:21:51 +0800

    update

--- src/com/fisher/Tick.java
+++ src/com/fisher/Tick.java
@@ -1,5 +1,11 @@
 package com.fisher;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by nick on 7/7/15.
  */
@@ -18,6 +24,25 @@ public class Tick {
         this.bid  = b;
         this.ask  = a;
         System.out.println("new tick created: " + this.time + " bid: " +this.bid + " ask: " + this.ask);
+        this.saveToFile();
+    }
+    public void saveToFile() {
+        String path = "/Users/nick/Documents/fisher/";
+        String fileName = "IB50DiffTick";
+
+        Date currentDate = new Date(this.time * 1000); // since this.time is in second, not millisecond
+        DateFormat df = new SimpleDateFormat("yyyyMMdd");
+        fileName = fileName + df.format(currentDate) + ".csv";
+
+        FileWriter fw = null;
+        try{
+            fw = new FileWriter(path + fileName, true);
+            fw.write(this.time + ", " + this.bid + ", " + this.ask + "\n");
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
